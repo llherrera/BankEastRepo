9d4542e4ffe258486748428cdb3826b0a9d8dc0c
Leonardo <leonardoh050@gmail.com>
2022-11-13 10:32:06 -0500

    port on db connection

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -4,6 +4,7 @@ import {DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT} from './index.js'
 export const create = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
+    port: DB_PORT,
     password: DB_PASS,
     database: DB_NAME
 })
