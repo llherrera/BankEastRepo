8b7c98a8ef91fc97f2a7de6f64ddc426dba1e176
Leonardo <leonardoh050@gmail.com>
2022-11-13 12:45:35 -0500

    .

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql';
 import {DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT} from './index.js'
 
-export const create = await mysql.createConnection({
+export const create = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
+    port: DB_PORT,
     password: DB_PASS,
     database: DB_NAME
 })
