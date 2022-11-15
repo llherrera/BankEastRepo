9dd1d10ef124743114e8c03428c1b0e5713b4098
Leonardo <leonardoh050@gmail.com>
2022-11-15 14:57:18 -0500

    without bcrypt

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -20,8 +20,8 @@ export const validationAndMake = async (req, res) => {
         if (owner.name != nombre || owner.email != email ) return res.status(400).json({message: 'User do not have this card'})
         if (card.owner != nombre) return res.status(400).json({message: 'User do not have this card'})
         if (card.card_type_id != mdPago || card.card_franchise_id != franquicia) return res.status(400).json({message: 'Bad type or franchise'})
-        //if (card.exp_month != expMonth || card.exp_year != expYear || card.cvv != cv) return res.status(400).json({message: 'Wrong parameters'})
-        if (!bcrypt.confirmPassword(expMonth, card.exp_month) || !bcrypt.confirmPassword(expYear, card.exp_year) || !bcrypt.confirmPassword(cv, card.cvv)) return res.status(401).json({ message: 'Wrong parameters' })
+        if (card.exp_month != expMonth || card.exp_year != expYear || card.cvv != cv) return res.status(400).json({message: 'Wrong parameters'})
+        //if (!bcrypt.confirmPassword(expMonth, card.exp_month) || !bcrypt.confirmPassword(expYear, card.exp_year) || !bcrypt.confirmPassword(cv, card.cvv)) return res.status(401).json({ message: 'Wrong parameters' })
         let discont
         if (mdPago == 1) discont = nroCuotas
         else discont = 1
