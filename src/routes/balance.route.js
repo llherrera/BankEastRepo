a0bbe3cfd841927c9c4deaf333944e4b198542f1
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-17 14:19:35 -0500

    C:/Program Files/Git/makepay and /checkbalance with POST method

--- src/routes/balance.route.js
+++ src/routes/balance.route.js
@@ -2,7 +2,7 @@ import * as balanceController from '../controllers/balance.controller.js'
 import { Router } from "express";
 const router = Router()
 
-router.get('/', balanceController.checkingBalance)
-router.get('/card', balanceController.checkCardBalance)
+router.use('/', balanceController.checkingBalance)
+router.use('/card', balanceController.checkCardBalance)
 
 export default router
