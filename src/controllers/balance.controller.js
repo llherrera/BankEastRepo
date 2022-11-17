d51837f3c37cd64dbc03722eba9f55ec4a2b6a55
Leonardo <leonardoh050@gmail.com>
2022-11-16 21:32:23 -0500

    send balance without id

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -6,10 +6,11 @@ export const checkingBalance = async (req, res) => {
     if (!nombre || !id || !nroTarjetas) return res.status(400).json({ message: 'Missing parameters' });
     
     try {
-        const saldos3 = await Card.find({$or: nroTarjetas.map(i => ({
+        let saldos = await Card.find({$or: nroTarjetas.map(i => ({
             card_number:i
         }))}, "amount")
-        return res.status(200).json({message: 'OK', saldos3})
+        saldos = saldos.map(i => i.amount)
+        return res.status(200).json({message: 'OK', saldos})
     } catch(err){
         return res.status(500).json({err})
     }
