8ca812694a78fe9d21083232309c997b5be054c9
Leonardo <leonardoh050@gmail.com>
2022-11-19 15:36:53 -0500

    changes in routes in East

--- src/models/card.model.js
+++ src/models/card.model.js
@@ -14,4 +14,4 @@ const cardSchema = new Schema({
   card_number: { type: Number, unique: true },
 });
 
-export default model('CardWestern', cardSchema);
+export default model('Card', cardSchema);
--- src/routes/balance.route.js
+++ src/routes/balance.route.js
@@ -2,7 +2,7 @@ import * as balanceController from '../controllers/balance.controller.js'
 import { Router } from "express";
 const router = Router()
 
-router.get('/', balanceController.checkingBalance)
+router.post('/', balanceController.checkingBalance)
 // router.use('/', balanceController.checkCardBalance)
 
 export default router
